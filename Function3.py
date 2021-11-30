752067ed68083bb93c437f037384e784d48e0b18
papasmf1 <papasmf1@gmail.com>
2021-11-30 16:49:26 +0900

    람다 함수 연습

--- Function3.py
+++ Function3.py
@@ -35,3 +35,24 @@ def union(*ar):
 #호출(중단점, Break Point)
 print( union("HAM","EGG") )
 print( union("HAM","EGG","SPAM") )
+
+#정의되지 않은 인자 처리(필수, 옵션도 섞여 있는 경우)
+#내부에서 딕셔너리(dict)
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL
+
+#호출
+print( userURIBuilder("naver.com", "80", id="kim", password="1234") )
+print( userURIBuilder("naver.com", "80", id="kim", password="1234", 
+    name="mike", age="30") )
+
+#람다 함수(간단하게 함수를 정의, 익명함수) 
+g = lambda x,y:x*y 
+print( g(3,4) )
+print( g(5,6) )
+#메모리에 있는 변수, 함수를 딕셔너리 
+print( globals() )
+

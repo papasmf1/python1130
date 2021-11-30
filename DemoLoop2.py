662c34066eb79d3467b6f8b054de2046c796b274
papasmf1 <papasmf1@gmail.com>
2021-11-30 11:50:29 +0900

    오늘 화요일 작업 완료

--- DemoLoop2.py
+++ DemoLoop2.py
@@ -5,3 +5,12 @@ lst = [1,2,3]
 for i in lst:
     print(i)
 
+
+print("---break구문---")
+lst = [1,2,3,4,5,6,7,8,9,10]
+#프리즌 프레이크(탈옥)
+for item in lst:
+    if item > 5:
+        break
+    print("Item:{0}".format(item))
+

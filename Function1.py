5c46a0c9035d09a47fdd9639dbc40d1b20abe2de
papasmf1 <papasmf1@gmail.com>
2021-11-30 14:32:11 +0900

    교집합 함수 연습

--- Function1.py
+++ Function1.py
@@ -1,6 +1,7 @@
 # Function1.py 
 #함수를 정의
 def setValue(newValue):
+    #함수 내부에서 초기화하면 지역변수(Local Variable)
     x = newValue
     print("함수내부:", x)
 
@@ -9,4 +10,22 @@ def setValue(newValue):
 result = setValue(5)
 print(result)
 
+#다중의 값을 리턴
+def swap(x,y):
+    return y,x 
 
+#호출
+result = swap(5,6)
+print(result)
+print( result[0], result[1] )
+
+#디버깅연습(교집합문자를 리턴)
+def union(prelist, postlist):
+    result = []
+    for x in prelist:
+        if x in postlist and x not in result:
+            result.append(x)
+    return result 
+
+#호출
+print( union("HAM", "SPAM") )

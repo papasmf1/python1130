178f5a2b855664e111f5f3ff9cb3e12ee6c7a7c8
papasmf1 <papasmf1@gmail.com>
2021-12-02 13:23:55 +0900

    문자열 연습2

--- DemoStr.py
+++ DemoStr.py
@@ -6,4 +6,21 @@ for name in names:
     print("안녕하세요 {0}님 추운 겨울입니다.".format(name))
     print("=" * 40)
 
+strA = "python is very powerful"
+print( len(strA) )
+print( strA.capitalize() )
+print( strA.count("p") )
+print( strA.count("p", 7) )
+print( "demo.ppt".endswith("ppt") )
+print( "MBC2580".isalnum() )
+print( "MBC:2580".isalnum() )
+print( "2580".isdecimal() )
 
+strB = "  spam and ham  "
+print(strB)
+result = strB.strip()
+print(result)
+
+
+
+ 

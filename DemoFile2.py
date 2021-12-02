1a4ce2af00e4daaa6d43e4f467ade264ed2fef56
papasmf1 <papasmf1@gmail.com>
2021-12-02 11:35:13 +0900

    파일 연습4

--- DemoFile2.py
+++ DemoFile2.py
@@ -8,8 +8,24 @@ print("{0:e}".format(4/3))
 print("{0:f}".format(4/3))
 print("{0:.2f}".format(4/3))
 
-#파일에 읽기와 쓰기 
-f = open("c:\\work\\demo.txt", "wt", encoding="utf-8")
+#파일에 읽기와 쓰기(한글을 읽기와 쓰기를 하는 경우 인코딩 지정) 
+f = open("c:/work/demo.txt", "wt", encoding="utf-8")
 f.write("한글\nabcd\n세번째\n")
 f.close()
+
+#읽기 
+f = open("c:\\work\\demo.txt", "rt", encoding="utf-8")
+#처음부터 끝까지 읽어서 하나의 str변수로 리턴 
+print( f.read() )
+print("어디쯤:", f.tell() )
+#처음으로 가기(리셋)
+f.seek(0)
+#한번에 한줄씩 읽기 
+print( f.readline(), end="" )
+print( f.readline(), end="" )
+f.seek(0)
+#전체를 읽어서 리스트로 리턴
+result = f.readlines()
+print(result)
+
 

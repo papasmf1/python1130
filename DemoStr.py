38ab57e8ed973fe8880732aa4da8a6e9a9e4ada9
papasmf1 <papasmf1@gmail.com>
2021-12-02 14:14:18 +0900

    정규 표현식 사용

--- DemoStr.py
+++ DemoStr.py
@@ -27,7 +27,37 @@ print(lst)
 print("---다시 하나로 합치기---")
 print( " ".join(lst) )
 
+#정규표현식 
+import re 
 
+string = "<<< data << data2"
+#서브스트링("패턴", "", 데이터)
+result = re.sub("<<<","", string)
+print(result)
 
+#특정 문자열 패턴:숫자가 0번에서 N번 th 
+result = re.match("[0-9]*th", "35th")
+result2 = re.search("[0-9]*th", "35th")
+#매칭 오브젝트 
+print(result)
+print(result2)
+#약간 가동
+print( result.group() )
 
- 
+#함정을 추가:match는 정확하게 일치 
+#특정한 규칙을 정의(패턴)
+result = re.match("[0-9]*th", "  35th")
+print(result)
+#search는 내부에 있으면(검색) 
+result2 = re.search("[0-9]*th", "  35th")
+print(result2)
+
+#년도를 검색 
+result = re.match("\d{4}", "올해는 2021년입니다.")
+print(result)
+result = re.search("\d{4}", "올해는 2021년입니다.")
+print( result.group() )
+
+#우편번호를 검색
+result = re.search("\d{5}", "우리 동네는 52300입니다.")
+print( result.group() )

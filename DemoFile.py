59ed01c1a36cea866982715b0e18c1389d606933
papasmf1 <papasmf1@gmail.com>
2021-12-02 10:50:56 +0900

    파일 연습3

--- DemoFile.py
+++ DemoFile.py
@@ -8,4 +8,23 @@ f = open("c:\\work\\demo.txt", "wt")
 print("파일에 쓰는 작업", file=f)
 f.close()
 
+#문자열을 결합하는 경우
+url = "http://www.naver.com/?page=" + str(1) 
+print(url)
+
+#문자열 정렬
+for x in range(1,6):
+    print(x,"*",x,"=",x*x)
+
+print("---오른쪽 정렬---")
+for x in range(1,6):
+    print(x,"*",x,"=",str(x*x).rjust(3) )
+
+
+print("---0으로 채우기---")
+for x in range(1,6):
+    print(x,"*",x,"=",str(x*x).zfill(3) )
+
+
+
 
